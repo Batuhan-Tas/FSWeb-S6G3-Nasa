2caeba1a665171a2ded0fcb24942a3a60d2f7235
Batuhan <batuhan.tas@yahoo.com>
2023-06-08 02:44:05 +0300

    S6G4 Styling Bolum 2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,72 @@
-import React, { useState , useEffect } from "react";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-import Header from './header';
-import Footer from './footer';
-
-
-
-
-
-
-
-
-
-
-
-
+import Header from "./header";
+import Footer from "./footer";
 
 function App() {
-  const [nasaData,setNasaData] = useState({ });
+  const [nasaData, setNasaData] = useState({});
 
   function getAxios() {
-    
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=xFQ2LBibm3wwPtXg7AryBPfOpv0HeKtdF0WRzsQg&date=2023-04-26&thumbs=true')
-    .then(res =>{
-      
-      setNasaData(res.data)
-            
-    })
-    .catch(error =>{
-      console.log('Error');
-    })
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=xFQ2LBibm3wwPtXg7AryBPfOpv0HeKtdF0WRzsQg&date=2023-04-26&thumbs=true"
+      )
+      .then((res) => {
+        setNasaData(res.data);
+      })
+      .catch((error) => {
+        console.log("Error");
+      });
   }
 
   useEffect(() => {
     getAxios();
-  },[]);
-  
+  }, []);
+
   return (
     <div className="App">
-      
-        <Header/>
+      <>
+        <div className="menu-left menu-basis vertical-write">
+          ASTRONOMY PICTURE OF THE DAY
+        </div>
+        <div className="column grow2">
+          <div className="grow">BUBBLE NEBULA</div>
+          <div className="bottom column">
+            <div className="grow2"></div>
+            <div className="grow3">
+              {nasaData && nasaData.media_type}
+              {nasaData && nasaData.service_version}
+              Designed by Rob Robertson
+            </div>
+          </div>
+        </div>
+        <div className="column grow3">
+          <div className="grow">
+            <div className="grow">
+              <span>
+                APOD
+                <b>{nasaData && nasaData.title} </b>
+                <br />
+                <img src={nasaData.url} alt={"Image of the Day !"} />
+              </span>
+            </div>
+            <div className="menu-right menu-basis">
+              <p>Date: {nasaData && nasaData.date}</p>
+            </div>
+          </div>
+          <div className="bottom column">
+            <div className="grow2 spread">
+              TYPE DISTANCE CONSTELLATION CATEGORY{" "}
+            </div>
+            <div className="grow3">
+              <p>{nasaData && nasaData.explanation}</p>
+            </div>
+          </div>
+        </div>
+      </>
+
+      {/*  <Header/>
         
         <p>Date: {nasaData && nasaData.date}</p>
         <p>{nasaData && nasaData.explanation}</p>
@@ -51,9 +78,7 @@ function App() {
         <p>{nasaData && nasaData.media_type}</p>
         <p>{nasaData && nasaData.service_version}</p>
 
-        <Footer/>    
-        
-      
+        <Footer/>     */}
     </div>
   );
 }
